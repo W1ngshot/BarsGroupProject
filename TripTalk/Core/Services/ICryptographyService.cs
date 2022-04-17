afd7ddd4252efe85ae51aa65be5fe244584f9042
Sxizman <102158778+Sxizman@users.noreply.github.com>
2022-04-17 05:30:17 +0300

    password encryption added (#1)
    * password encryption added

---
+++ TripTalk/Core/Services/ICryptographyService.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Services;
+
+public interface ICryptographyService
+{
+    public Task<string> EncryptPasswordAsync(string password, string salt);
+
+    public string GenerateSalt();
+}
