75d0f85acbfb5e79deb86df13f3ace4b4871820f
Sxizman <germanbikbow@gmail.com>
2022-04-17 04:58:57 +0300

    password encryption added

---
+++ TripTalk/Core/Services/CryptographyService.cs
@@ -0,0 +1,21 @@
+﻿using Konscious.Security.Cryptography;
+using System.Text;
+
+namespace Core.Services
+{
+    public class CryptographyService : ICryptographyService
+    {
+        public async Task<string> EncryptPasswordAsync(string password, byte[] salt)
+        {
+            var encrypter = new Argon2d(Encoding.Unicode.GetBytes(password));
+
+            encrypter.Salt = salt;
+            encrypter.Iterations = 40;
+            encrypter.DegreeOfParallelism = 16;
+            encrypter.MemorySize = 8192;
+
+            var encodedBytes = await encrypter.GetBytesAsync(128);
+            return Convert.ToBase64String(encodedBytes);
+        }
+    }
+}
