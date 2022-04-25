70dfc23730ba8f752ebd9622d4bb3aef7ac34f96
Sxizman <102158778+Sxizman@users.noreply.github.com>
2022-04-25 10:38:17 +0300

    Article validator added (#3)
    * Article validator added

--- TripTalk/Core/Validators/ArticleValidator.cs
+++ TripTalk/Core/Validators/ArticleValidator.cs
@@ -7,6 +7,31 @@ public class ArticleValidator : AbstractValidator<Article>
 {
     public ArticleValidator()
     {
-        //TODO реализовать
+        RuleFor(a => a.Title)
+            .NotEmpty()
+            .WithMessage(ValidationMessages.EmptyArticleTitle);
+        RuleFor(a => a.Title)
+            .MinimumLength(10)
+            .WithMessage(ValidationMessages.TooShortArticleTitle);
+        RuleFor(a => a.Title)
+            .MaximumLength(60)
+            .WithMessage(ValidationMessages.TooLongArticleTitle);
+        RuleFor(a => a.Title)
+            .Matches(@"^[\w\s,.!?""':;`-()]+$")
+            .WithMessage(ValidationMessages.ArticleTitleContainsWrongSymbols);
+
+        RuleFor(a => a.ShortDescription)
+            .MaximumLength(400)
+            .WithMessage(ValidationMessages.TooLongArticleDescription);
+
+        RuleFor(a => a.Text)
+            .NotEmpty()
+            .WithMessage(ValidationMessages.EmptyArticleText);
+        RuleFor(a => a.Text)
+            .MinimumLength(500)
+            .WithMessage(ValidationMessages.TooShortArticleText);
+        RuleFor(a => a.Text)
+            .MaximumLength(10000)
+            .WithMessage(ValidationMessages.TooLongArticleText);
     }
 }
