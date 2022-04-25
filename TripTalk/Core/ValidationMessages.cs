6a8b420a7a783fa4860e01460cc40d298fd67ef1
Sxizman <germanbikbow@gmail.com>
2022-04-25 10:23:42 +0300

    Article validator added

--- TripTalk/Core/ValidationMessages.cs
+++ TripTalk/Core/ValidationMessages.cs
@@ -9,4 +9,13 @@ public class ValidationMessages
     public const string EmptyEmail = "Поле электронной почты не может быть пустым";
     public const string IncorrectEmail = "Почта введена неверно";
     public const string LoginOrEmailAlreadyExists = "Данный логин или почта уже были зарегистрированы";
+
+    public const string EmptyArticleTitle = "Введите название статьи";
+    public const string TooShortArticleTitle = "Слишком короткое название";
+    public const string TooLongArticleTitle = "Слишком длинное название";
+    public const string ArticleTitleContainsWrongSymbols = "Недопустимые символы в названии";
+    public const string TooLongArticleDescription = "Слишком длинное описание";
+    public const string EmptyArticleText = "Введите текст статьи";
+    public const string TooShortArticleText = "Текст статьи должен содержать не менее 500 символов";
+    public const string TooLongArticleText = "Текст статьи должен содержать не более 10 тысяч символов";
 }
