1d328f628a2e7ddfeca39c295648a1cfb840a2ae
W1ngshot <kirill-samsonov@bk.ru>
2022-04-30 19:58:43 +0300

    file updated to match main branch

--- TripTalk/Data/Repositories/ArticleRepository.cs
+++ TripTalk/Data/Repositories/ArticleRepository.cs
@@ -17,20 +17,18 @@ public class ArticleRepository : IArticleRepository
     }
 
     //TODO использовать модели, чтобы не передавать весь элемент
-    //TODO использовать свойства статьи Views и Rating вместо заглушки
-    public async Task<List<Article>> GetCategoryArticlesAsync(Category category, Period period, int count, int firstIndex)
+    public async Task<List<Article>> GetOrderedArticlesAsync(Category category, Period period, int count, int firstIndex)
     {
         var unorderedArticles = _context.Articles
             .AsNoTracking()
-            .Include(article => article.User)
-            .Include(article => article.Tags)
             .Where(article => (DateTime.UtcNow - article.UploadDate).TotalDays < (int)period);
 
         var orderedArticles = category switch
         {
             Category.Popular => unorderedArticles.OrderByDescending(article => article.Views),
             Category.Last => unorderedArticles.OrderByDescending(article => article.UploadDate),
-            Category.Best => unorderedArticles.OrderByDescending(article => article.Rating)
+            Category.Best => unorderedArticles.OrderByDescending(article => article.Rating),
+            _ => throw new Exception(ErrorMessages.InvalidCategoryValue)
         };
 
         var articleModelList = await orderedArticles
@@ -48,9 +46,34 @@ public class ArticleRepository : IArticleRepository
             UserId = entity.UserId,
             PreviewPictureLink = entity.AssetLink,
             Rating = entity.Rating,
-            Views = entity.Views,
-            UserNickname = entity.User.Nickname,
-            TagNames = entity.Tags?.Select(tag => tag.Name).ToList() ?? new List<string>()
+            Views = entity.Views
+        }).ToList();
+    }
+
+    public async Task<List<Article>> GetFilteredArticlesAsync(string searchLine, List<string>? tags, int count, int firstIndex)
+    {
+        var filteredArticles = _context.Articles
+            .AsNoTracking()
+            .Where(article => article.Title.Contains(searchLine));
+        if (tags is not null)
+            filteredArticles = filteredArticles.Where(article => article.Tags.Any(tag => tags.Contains(tag.Name)));
+
+        var articleModelList = await filteredArticles
+            .Skip(firstIndex)
+            .Take(count)
+            .ToListAsync();
+
+        return articleModelList.Select(entity => new Article
+        {
+            Id = entity.Id,
+            Title = entity.Title,
+            ShortDescription = entity.ShortDescription,
+            Text = entity.Text,
+            UploadDate = entity.UploadDate,
+            UserId = entity.UserId,
+            PreviewPictureLink = entity.AssetLink,
+            Rating = entity.Rating,
+            Views = entity.Views
         }).ToList();
     }
 
@@ -81,7 +104,7 @@ public class ArticleRepository : IArticleRepository
     public async Task<Article> GetArticleByIdAsync(int id)
     {
         var entity = await _context.Articles.FirstOrDefaultAsync(article => article.Id == id) ??
-                             throw new Exception("Данного пользователя не существует"); //TODO подумать как исправить exception
+            throw new Exception(ErrorMessages.MissingUser); //TODO подумать как исправить exception
         return new Article
         {
             Id = entity.Id,
@@ -96,7 +119,7 @@ public class ArticleRepository : IArticleRepository
         };
     }
 
-    public async Task CreateArticleAsync(Article article)
+    public async Task AddArticleAsync(Article article)
     {
         var entity = new ArticleDbModel
         {
@@ -115,7 +138,7 @@ public class ArticleRepository : IArticleRepository
     public async Task UpdateArticleAsync(Article article)
     {
         var entity = await _context.Articles.FirstOrDefaultAsync(a => a.Id == article.Id) ??
-                     throw new Exception("Данного пользователя не существует"); //TODO подумать как исправить exception
+            throw new Exception(ErrorMessages.MissingUser); //TODO подумать как исправить exception
 
         entity.Title = article.Title;
         entity.ShortDescription = article.ShortDescription;
@@ -123,5 +146,13 @@ public class ArticleRepository : IArticleRepository
         entity.AssetLink = article.PreviewPictureLink;
         entity.Rating = article.Rating;
         entity.Views = article.Views;
+    }
+
+    public async Task RemoveArticleAsync(int id)
+    {
+        var entity = await _context.Articles.FirstOrDefaultAsync(a => a.Id == id) ??
+            throw new Exception(ErrorMessages.MissingArticle);
+
+        _context.Articles.Remove(entity);
     }
 }
